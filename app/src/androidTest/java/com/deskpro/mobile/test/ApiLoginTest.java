87208c3ec5b56f8c0a1b7602ff9ae329014a56f2
Christopher Nadeau <chris@nadeau.ws>
2014-03-22 10:52:26 +0000

    add testing stuff

---
+++ app/src/androidTest/java/com/deskpro/mobile/test/ApiLoginTest.java
@@ -0,0 +1,11 @@
+package com.deskpro.mobile.test;
+
+import android.test.InstrumentationTestCase;
+
+public class ApiLoginTest extends InstrumentationTestCase
+{
+	public void testTrue() {
+		assertTrue(true);
+		assertTrue(false);
+	}
+}
