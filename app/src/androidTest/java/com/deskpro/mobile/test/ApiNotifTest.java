7e4dd12e9b95c6bfef7852a38a4319260cd93888
Christopher Nadeau <chris@nadeau.ws>
2014-03-22 11:36:55 +0000

    add blank testing value res

--- app/src/androidTest/java/com/deskpro/mobile/test/ApiNotifTest.java
+++ app/src/androidTest/java/com/deskpro/mobile/test/ApiNotifTest.java
@@ -2,10 +2,9 @@ package com.deskpro.mobile.test;
 
 import android.test.InstrumentationTestCase;
 
-public class ApiLoginTest extends InstrumentationTestCase
+public class ApiNotifTest extends InstrumentationTestCase
 {
 	public void testTrue() {
-		assertTrue(true);
-		assertTrue(false);
+
 	}
 }
